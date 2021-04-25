53a6cdbd52410a255e85894af8194857bdf7ad59
samueltanner <samuel.m.s.tanner@gmail.com>
2021-04-24 17:25:45 -0700

    tries a few methods of getting the correct data to show in group show view

--- src/views/GroupsShow.vue
+++ src/views/GroupsShow.vue
@@ -10,6 +10,11 @@
       <h1>Group Members:</h1>
       <div v-for="member in group.members" :key="member.id">
         <p>{{ member.username }}</p>
+
+        <!-- <h2>{{ getUserPortfolio(member) }}</h2>  -->
+        <!-- <div v-for="transactions in user_transactions" :key="transactions.id">
+          <p>{{ transactions }}</p>
+        </div> -->
         <div v-for="transaction in member.transactions" :key="transaction.id">
           {{ transaction.symbol }} @ {{ transaction.purchase_price }}
         </div>
@@ -25,12 +30,12 @@ export default {
   data: function () {
     return {
       group: {},
+      user_transactions: {},
     };
   },
   created: function () {
     this.showGroup();
   },
-
   methods: {
     showGroup: function () {
       axios.get("/api/groups/" + this.$route.params.id).then((response) => {
@@ -38,9 +43,12 @@ export default {
         this.group = response.data;
       });
     },
-    getUserPortfolio: function (user) {
-      axios.get("/api/user/" + user.id);
-    },
+    // getUserPortfolio: function (user) {
+    //   axios.get("/api/users/" + user.id).then((response) => {
+    //     console.log(response.data);
+    //     this.user_transactions = response.data;
+    //   });
+    // },
   },
 };
 </script>
